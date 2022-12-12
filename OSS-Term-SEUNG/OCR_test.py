946c4515c2c7169901b6a716bc5649d4bea85997
MuoN0x0 <112800160+MuoN0x0@users.noreply.github.com>
2022-12-13 03:51:26 +0900

    crop image, find test and name
    crop the title of test and name,
    and ocr find these.

--- OSS-Term-SEUNG/OCR_test.py
+++ OSS-Term-SEUNG/OCR_test.py
@@ -1,14 +1,24 @@
 import pytesseract
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread('./image.png')
-h, w, c = image.shape
-print(h, w, c)
+config = ('-l kor+eng --oem 3 --psm 4')
+img = cv2.imread('./omr.png', cv2.IMREAD_GRAYSCALE)
+print('이미지 그레이처리----------------')
 
-cv2.imshow("Image", image)
+#omr_testName_gray = cv2.imread('./omr.png', cv2.COLOR_BGR2GRAY)
+omr_testName_gray = img[0:160, 0:700]
+omr_testName_gray=cv2.resize(omr_testName_gray,dsize=(0,0),fx=2.5,fy=2.5,interpolation=cv2.INTER_AREA)
+cv2.imshow("testName", omr_testName_gray)
+testName = pytesseract.image_to_string(omr_testName_gray, config=config)
+print(testName)
+print('이름----------------')
+
+omr_name = img[180:250, 50:320]
+omr_name=cv2.resize(omr_name,dsize=(0,0),fx=2.5,fy=2.5,interpolation=cv2.INTER_AREA)
+cv2.imshow("Name", omr_name)
+name = pytesseract.image_to_string(omr_name, config=config)
+print(name)
+
 cv2.waitKey()
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-text = pytesseract.image_to_string(rgb_image, lang='kor')
-print(text)
